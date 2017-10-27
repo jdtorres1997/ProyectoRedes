284e8f84058536a8c6fde88d24037f1ab42251f6
jdtorres1997 <juan.torres.canon@correounivalle.edu.co>
2017-10-27 17:58:01 -0500

    Update IntranetHeroku.py

--- IntranetHeroku.py
+++ IntranetHeroku.py
@@ -6,3 +6,5 @@
 #1. Permitira obtener informacion que este almacenado en la base de datos de heroku, como el estado de las
 # descargas y de la maquina en la intranet
 #2. Podra obtener una lista de los magnets links de las descargas que aun no han sido iniciadas.
+# Protocolo
+# SEND DOWNLOAD <magnet-link>
