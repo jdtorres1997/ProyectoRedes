d0f745f00415bf00feed826dc9232e9efdf517c1
juanjvargas <juan.jose.vargas@correounivalle.edu.co>
2017-10-27 17:21:35 -0500

    Update DescargasIntranet.py

--- DescargasIntranet.py
+++ DescargasIntranet.py
@@ -5,6 +5,6 @@
 #Este script que estara ubicado en la maquina de la intranet tendra las siguientes funcionalidades:
 #1. Se encargara de generar una conexion desde la intranet a la pasarela
 #2. Podra enviar informacion desde la intranet a la pasarela para tener la informacion actualizada
-#del estado de la descarga y la maquina de la intranet donde se esta ejecutando el script
+#del estado de la descarga
 #3. Podra comunicarse con la pasarela para saber si hay descargas pendientes o no
 #4. De existir utilizara transmission con el magnet link e iniciara la descarga
