43314c774883ec1a22b0ff67e939b7a6b63a8c72
jdtorres1997 <juan.torres.canon@correounivalle.edu.co>
2017-10-27 17:44:37 -0500

    Update MonitoreoIntranet.py

--- MonitoreoIntranet.py
+++ MonitoreoIntranet.py
@@ -2,4 +2,9 @@
 
 
 #MonitoreoIntranet
-#Programa que gestiona la informacion de la maquina de la intranet
+#Programa que gestiona la informacion de la maquina de la intranet, cuenta con las siguentes funcionalidades:
+#1 Utiliza el programa RESTMonitoring para consultar información relacionada el funcionamiento de la maquina sobre la que se
+# ejecuta, dentro de la intranet.
+#2 Envia los datos de la maquina consultada a la pasarela en Heroku para mantener actualizada dicha informacion.
+#Protocolo de comunicación:
+#SEND INFOPC (<item-name>:<item-information>) 
