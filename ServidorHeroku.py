a8bfce2aae8387aec01e94f5903734463e36af79
jdtorres1997 <juan.torres.canon@correounivalle.edu.co>
2017-10-27 18:00:05 -0500

    Update ServidorHeroku.py

--- ServidorHeroku.py
+++ ServidorHeroku.py
@@ -5,3 +5,6 @@
 #Este script tendra las siguientes funcionlidades:
 #1. Puede solicitar informacion de la maquina o las decargas.
 #2. Puede iniciar una descarga solicitada.
+# Protocolo
+# Para la respuesta al cliente, se deberia utilizar solo HTTP, ya que si enviamos nuestro propio protocolo, el navegador que hace
+# la solicitud no sabría como interpretarlo.
