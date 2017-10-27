e3213d095a88303ec567a6670b59ec6d10774973
juanjvargas <juan.jose.vargas@correounivalle.edu.co>
2017-10-27 17:19:52 -0500

    Update IntranetHeroku.py

--- IntranetHeroku.py
+++ IntranetHeroku.py
@@ -3,4 +3,6 @@
 
 #Intranetheroku
 #Este script que estara en heroku tendra las siguientes funcionalidades:
-#1. 
+#1. Permitira obtener informacion que este almacenado en la base de datos de heroku, como el estado de las
+# descargas y de la maquina en la intranet
+#2. Podra obtener una lista de los magnets links de las descargas que aun no han sido iniciadas.
