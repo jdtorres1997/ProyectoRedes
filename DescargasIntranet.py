664e0d52306e8778d1c9cfbf4a0b5e573db55735
juanjvargas <juan.jose.vargas@correounivalle.edu.co>
2017-10-27 16:48:11 -0500

    Update DescargasIntranet.py

--- DescargasIntranet.py
+++ DescargasIntranet.py
@@ -2,4 +2,8 @@
 
 
 #DescargasIntranet
-#
+#1. Se encargara generar una conexion desde la intranet a la pasarela
+#2. Podra enviar informacion desde la intranet a la pasarela para tener la informacion actualizada
+#del estado de la descarga y la maquina de la intranet donde se esta ejecutando el scritp
+#3. Podra comunicarse con la pasarela para saber si hay descargas epndientes o no
+#4. De existir utilizara transmission con el magnet link e iniciara la descarga
