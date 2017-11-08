3ebb61600f96cf962485f4793dbdfe6737d51690
juanjvargas <juan.jose.vargas@correounivalle.edu.co>
2017-11-08 16:19:18 -0500

    Add files via upload

--- IntranetHeroku.py
+++ IntranetHeroku.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
-
-
+import requests
 #Intranetheroku
 #Este script que estara en heroku tendra las siguientes funcionalidades:
 #1. Permitira obtener informacion que este almacenado en la base de datos de heroku, como el estado de las
@@ -8,3 +7,13 @@
 #2. Podra obtener una lista de los magnets links de las descargas que aun no han sido iniciadas.
 # Protocolo
 # SEND DOWNLOAD <magnet-link>
+
+#print("nombre\n")
+nombre = input()
+#print("magnetlink\n")
+magnetlink = input()
+estado= "pendiente"
+ms= nombre+","+magnetlink+","+estado
+url="https://proyectoredestorresvargas.herokuapp.com/descargar/"+ms
+response = requests.get(url)
+print(response)
